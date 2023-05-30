a14d5cb5b1bbcfc36075e1bdc02ac5c08747375a
ravikumar <ravikumarnaduvin@gmail.com>
2023-05-31 03:32:08 +0530

    updated bash script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 pip install -e .
-# pip install -r requirements.txt # comment after installing once.
+pip install -r requirements.txt # comment after installing once.
 # python metadata/create_species_data.py # uncomment to seperate the folder of species
 # python src/train.py #uncomment this line to train a model # uncomment to train model and save the result & checkpoint to checkpoint folder
 python webapp/app.py
